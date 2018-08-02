4380a88dcb6bcc47093e5344f67da698a4f20fdc
William Friedrich <willwall916@gmail.com>
2018-08-02 13:11:44 -0700

    Created pop-up for Hit Me btn

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
