926f67005737870d4ca7ba7808e5bb942e0a0611
William Friedrich <willwall916@gmail.com>
2018-08-03 10:15:42 -0700

    added a random number generator for targetValue

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 0
     @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         currentValue = lroundf(slider.value)
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is \(currentValue)"
+        let message = "The value of the slider is \(currentValue)" + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
