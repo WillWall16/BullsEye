09420b345cae65ceb242717bb96de3e0af62764d
William Friedrich <willwall916@gmail.com>
2018-08-05 20:07:52 -0700

    Created startOver function

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var round: UILabel!
     
     var currentValue: Int = 0
     var targetValue: Int = 0
+    var currentRound: Int = 0
+    var currentScore: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +28,14 @@ class ViewController: UIViewController {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        currentRound = currentRound + 1
         updateLabels()
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        score.text = String(currentScore)
+        round.text = String(currentRound)
     }
 
     @IBAction func showAlert() {
@@ -44,17 +51,21 @@ class ViewController: UIViewController {
         
         let message = "The value of the slider is \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe difference is \(difference)"
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in self.startNewRound()})
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver() {
+        currentScore = 0
+        currentRound = 0
+        startNewRound()
     }
 }
 
